ebbc376982326cb27a607d48741adfc9e1b1eef4
jmk8366 <jmk8366@naver.com>
2020-04-08 12:24:29 +0900

    200408

--- app/src/main/java/com/example/entrustment/MainFragment.java
+++ app/src/main/java/com/example/entrustment/MainFragment.java
@@ -13,7 +13,7 @@ public class MainFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.institute_paper, container, false);
+        View v = inflater.inflate(R.layout.approve_ent, container, false);
         return v;
     }
 }
